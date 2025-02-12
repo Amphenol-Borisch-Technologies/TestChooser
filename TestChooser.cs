5f5ca561e214b1f3b2b383f349c1e5dd6820e622
Phillip Smelt <phils@borisch.com>
2025-02-12 15:17:31 -0500

    2/12/25

--- TestChooser.cs
+++ TestChooser.cs
@@ -12,7 +12,7 @@ namespace TestChooser {
                 openFileDialog.InitialDirectory = @"C:\Program Files\ABT\Test\TestPlans\";
                 openFileDialog.Filter = "TestPlan Programs|*.exe";
 
-Choose:         if (openFileDialog.ShowDialog() != DialogResult.OK) {
+Choose: if (openFileDialog.ShowDialog() != DialogResult.OK) {
                     DialogResult dialogResult = MessageBox.Show(ActiveForm, $"Do you want to exit?{Environment.NewLine}{Environment.NewLine}", "Exit?", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
                     if (dialogResult == DialogResult.Yes) Application.Exit();
                     else goto Choose;
@@ -23,27 +23,34 @@ Choose:         if (openFileDialog.ShowDialog() != DialogResult.OK) {
                     while (testPlanNew.MainWindowHandle == IntPtr.Zero && iterations <= 60) {
                         // TODO: Soon; display visual progress bar.
                         Thread.Sleep(500);
-                        iterations++; // 60 iterations with 0.5 second sleeps = 30 seconds max.
                         testPlanNew.Refresh();
+                        iterations++; // 60 iterations with 0.5 second sleeps = 30 seconds max.
+                    }
+                    Cursor.Current = Cursors.Default;
+                    if (testPlanNew.MainWindowHandle == IntPtr.Zero) {
+                        _ = MessageBox.Show(ActiveForm, $"New TestPlan hasn't started.{Environment.NewLine}{Environment.NewLine}" +
+                        "Please contact Test Engineering.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Application.Exit();
                     }
 
                     Process testPlanOld = null;
                     Int32 testPlanOldPID = 0;
                     if (args.Length > 0) try { testPlanOldPID = Convert.ToInt32(args[0]); } catch { }
-                    if (testPlanNew.MainWindowHandle != IntPtr.Zero && testPlanOldPID != 0) {
+                    if (testPlanOldPID != 0) {
                         try {
                             testPlanOld = Process.GetProcessById(testPlanOldPID);
                             iterations = 0;
+                            Cursor.Current = Cursors.WaitCursor;
                             while (!testPlanOld.HasExited && iterations <= 60) {
                                 // TODO: Soon; display visual progress bar.
                                 Thread.Sleep(500);
-                                iterations++;
                                 testPlanOld.Refresh();
+                                iterations++; // 60 iterations with 0.5 second sleeps = 30 seconds max.
                             }
+                            Cursor.Current = Cursors.Default;
                         } catch { }
                     }
 
-                    Cursor.Current = Cursors.Default;
                     if (testPlanOld != null && !testPlanOld.HasExited) _ = MessageBox.Show(ActiveForm, $"Old TestPlan hasn't exited.{Environment.NewLine}{Environment.NewLine}" +
                         "Please contact Test Engineering.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     Application.Exit();
